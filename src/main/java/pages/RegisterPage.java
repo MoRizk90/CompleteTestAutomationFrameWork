bd9b534d033744904dcf2b1c194396d39e631fe5
Mohammed Rizk <Mo.Magdy.Rizk@gmail.com>
2019-04-04 01:00:57 +0200

    Added Registeration test cases with assertion

--- src/main/java/pages/RegisterPage.java
+++ src/main/java/pages/RegisterPage.java
@@ -52,18 +52,24 @@ public class RegisterPage extends pageBase{
 	@FindBy(id = "register-button")
 	WebElement register_button;
 	
+	//For assertion
+	
+	@FindBy(xpath = "//div[@class='result']")
+	public WebElement registerationSuccessMsg;
+	
+	@FindBy(xpath = "//input[@value='Continue']")
+	public WebElement successContinueBTN;
+	
 	
 	
 	
 	@FindBy(xpath= "//h1[contains(text(),'Register')]")
 	WebElement registerTitle;
 	
-	public void registerNewUser(String gender, String Fname, String lname) {
+	public void registerNewUser(String gender, String Fname, String lname, String date, int month, String year, String email, String company, String testPassword) {
 		Select birth_day_drp_obj = new Select(the_day_of_birth_dropdown);
 		Select birth_month_drp_obj = new Select(the_month_of_birth_dropdown);
 		Select birth_year_drp_obj = new Select(the_year_of_birth_dropdown);
-		
-		
 		if (gender.toLowerCase().contentEquals("male")) {
 			click(Male_Radio_Button);
 			
@@ -73,7 +79,17 @@ public class RegisterPage extends pageBase{
 		
 		writeInElement(Fname, First_name_txtBox);
 		writeInElement(lname, Last_name_txtBox);
-		birth_day_drp_obj.selectByVisibleText(text);
+		birth_day_drp_obj.selectByVisibleText(date);
+		birth_month_drp_obj.selectByIndex(month);
+		birth_year_drp_obj.selectByVisibleText(year);
+		writeInElement(email, this.email);
+		writeInElement(company, company_txtbox);
+		click(newletter_checkbox);
+		writeInElement(testPassword, password_txtBox);
+		writeInElement(testPassword, confirm_password_txtBox);
+		click(register_button);
+		
+		
 	}
 	
 	public WebElement getRegisterTitle() {
--- src/test/java/tests/RegisterationTestCase.java
+++ src/test/java/tests/RegisterationTestCase.java
@@ -1,11 +1,18 @@
 package tests;
 
+import static org.testng.Assert.assertTrue;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import com.github.javafaker.*;
+import com.github.javafaker.DateAndTime;
+import com.github.javafaker.Faker;
 
 import pages.HomePage;
 import pages.RegisterPage;
@@ -14,15 +21,11 @@ public class RegisterationTestCase extends TestBase {
 	HomePage home_page_obj;
 	RegisterPage reg_page_obj;
 	Faker fakeObj = new Faker();
-	Da
-	/*
-	 * ana kont b7awel eny b7awel eny AGENERATE DATE nbest3mal el faker lib
-	 * 
-	 * 
-	 */
-	
 
-	
+
+
+
+
 	@Test
 	public void open_registeration_page() {
 		home_page_obj = new HomePage(testBaseDriver);
@@ -30,10 +33,30 @@ public class RegisterationTestCase extends TestBase {
 		wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("div.master-wrapper-page:nth-child(6) div.master-wrapper-content div.master-column-wrapper div.center-1 div.page.registration-page div.page-title > h1:nth-child(1)")));
 		System.out.println(testBaseDriver.getCurrentUrl());
 		Assert.assertTrue(testBaseDriver.getCurrentUrl().contains("register"));
-		fakeObj.
 	}
-	
-	
-	
-	
+
+	@SuppressWarnings("static-access")
+	@Test(dependsOnMethods = {"open_registeration_page"})
+	private void RegisterNewUserWithCorrectcredential() {
+		reg_page_obj = new RegisterPage(testBaseDriver);
+		String gender = "male" ;
+		String first_name = fakeObj.name().firstName();
+		String Last_name  = fakeObj.name().lastName();
+		Date theDate = fakeObj.date().future(10, TimeUnit.DAYS);
+		Calendar newCal =  Calendar.getInstance();
+		newCal.setTime(theDate);
+		String theDay = Integer.toString(newCal.get(newCal.DATE));
+		int TheMonth = newCal.get(newCal.MONTH);
+		String theYear = Integer.toString(newCal.get(newCal.YEAR));
+		String email = fakeObj.internet().emailAddress();
+		String company = fakeObj.company().name();
+		String testPassword = fakeObj.internet().password();
+		reg_page_obj.registerNewUser(gender, first_name, Last_name, theDay, TheMonth, theYear, email, company, testPassword);
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("register-continue")));
+		assertTrue(reg_page_obj.registerationSuccessMsg.getText().contentEquals("Your registration completed"));
+	}
+
+
+
+
 }
