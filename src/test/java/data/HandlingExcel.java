ec387dd92b6fae0ebf21112e586ccbcd5f7c245d
Mohammed Rizk <Mo.Magdy.Rizk@gmail.com>
2019-04-15 18:21:33 +0200

    updaating repo

---
+++ src/test/java/data/HandlingExcel.java
@@ -0,0 +1,107 @@
+package data;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.testng.annotations.DataProvider;
+
+public class HandlingExcel {
+	static FileInputStream excel_workbook_holder = null;
+
+	static FileInputStream targeted_excel_for_test ;
+	String file_Path = System.getProperty("user.dir") + "\\src\\test\\java\\data\\TestData.xlsx";
+
+	public FileInputStream Open_excel_sheet() {
+
+		File the_excel_file = new File(file_Path);
+		try {
+			targeted_excel_for_test = new FileInputStream(the_excel_file);
+		} catch (FileNotFoundException e) {
+			System.out.println(e.getMessage());
+			System.exit(0);
+		}	
+		return targeted_excel_for_test;
+
+	}
+
+	@DataProvider(name= "excelData")
+	public Object[][] getExcelData() throws IOException{
+		Open_excel_sheet();
+		XSSFWorkbook test_work_book = new XSSFWorkbook(targeted_excel_for_test);
+		XSSFSheet test_sheet = test_work_book.getSheetAt(0);
+		int TotalNumberofRow = (test_sheet.getLastRowNum() + 1);
+		int TotalNumberOfColumn = 4;
+		String[][] arrayExcelData =  new String[TotalNumberofRow][TotalNumberOfColumn];
+		for (int rows = 0; rows < TotalNumberofRow; rows++) {
+			for (int column = 0; column < TotalNumberOfColumn ; column++) {
+
+				XSSFRow row = test_sheet.getRow(rows);
+				arrayExcelData[rows][column] = row.getCell(column).toString();
+
+			}
+
+		}
+//		test_work_book.close();
+		System.out.println(arrayExcelData);
+		
+		return arrayExcelData;
+		
+
+	}
+
+	public void write_data_into_excel(int row_count, String row_location, String enter_gender, String enter_first_name, String enter_last_name	, String enter_the_Day, String enter_The_Month,  String enter_the_Year,String enter_test_email, String	enter_test_company, String	enter_test_Password
+			) throws IOException {
+		Open_excel_sheet();
+		XSSFWorkbook test_work_book = new XSSFWorkbook(targeted_excel_for_test);
+		XSSFSheet test_sheet = test_work_book.getSheetAt(0);
+		Map<String, Object[]> data = new TreeMap<String, Object[]>(); 
+		data.put("0", new Object[]{"gender", "first_name", "Last_name", "theDay", "TheMonth", "theYear", "email", "company", "testPassword"});
+		data.put(row_location, new Object[]{ enter_gender, enter_first_name, enter_last_name, enter_the_Day, enter_The_Month, enter_the_Year, enter_test_email, enter_test_company, enter_test_Password});
+		Set<String> keyset = data.keySet();
+		int rownum = row_count; 
+		for (String key : keyset) { 
+			// this creates a new row in the sheet 
+			
+			Row row = test_sheet.createRow(rownum); 
+			Object[] objArr = data.get(key); 
+			int cellnum = 0; 
+			for (Object obj : objArr) { 
+				// this line creates a cell in the next column of that row 
+				Cell cell = row.createCell(cellnum++); 
+
+				if (obj instanceof String) {
+					cell.setCellValue((String)obj); 
+				}
+				else if (obj instanceof Integer) {
+					cell.setCellValue((Integer)obj); 
+				}
+			} 
+			rownum++;
+		} 
+		try { 
+			File the_excel_file = new File(file_Path);
+			FileOutputStream out = new FileOutputStream(the_excel_file); 
+			test_work_book.write(out); 
+			out.close(); 
+//			test_work_book.close();
+			System.out.println("gfgcontribute.xlsx written successfully on disk."); 
+		} 
+		catch (Exception e) { 
+			e.printStackTrace(); 
+		} 
+	} 
+
+
+}
+
--- src/test/java/tests/RegisterationTestCase.java
+++ src/test/java/tests/RegisterationTestCase.java
@@ -2,6 +2,7 @@ package tests;
 
 import static org.testng.Assert.assertTrue;
 
+import java.io.IOException;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
@@ -9,11 +10,12 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
-import com.github.javafaker.DateAndTime;
 import com.github.javafaker.Faker;
 
+import data.HandlingExcel;
 import pages.HomePage;
 import pages.RegisterPage;
 
@@ -21,8 +23,6 @@ public class RegisterationTestCase extends TestBase {
 	HomePage home_page_obj;
 	RegisterPage reg_page_obj;
 	Faker fakeObj = new Faker();
-
-
 
 
 
@@ -36,7 +36,7 @@ public class RegisterationTestCase extends TestBase {
 	}
 
 	@SuppressWarnings("static-access")
-	@Test(dependsOnMethods = {"open_registeration_page"})
+	@Test(dependsOnMethods = {"open_registeration_page"}, priority = 1, enabled = false)
 	private void RegisterNewUserWithCorrectcredential() {
 		reg_page_obj = new RegisterPage(testBaseDriver);
 		String gender = "male" ;
@@ -55,6 +55,44 @@ public class RegisterationTestCase extends TestBase {
 		wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("register-continue")));
 		assertTrue(reg_page_obj.registerationSuccessMsg.getText().contentEquals("Your registration completed"));
 	}
+	
+	@SuppressWarnings("static-access")
+	@Test(dependsOnMethods = {"open_registeration_page"}, priority = 1)
+	public void add_data_to_excel() throws IOException {
+		String gender = "male" ;
+		String first_name = fakeObj.name().firstName();
+		String Last_name  = fakeObj.name().lastName();
+		Date theDate = fakeObj.date().future(10, TimeUnit.DAYS);
+		Calendar newCal =  Calendar.getInstance();
+		newCal.setTime(theDate);
+		String theDay = Integer.toString(newCal.get(newCal.DATE));
+		String TheMonth = Integer.toString(newCal.get(newCal.MONTH));
+		String theYear = Integer.toString(newCal.get(newCal.YEAR));
+		String email = fakeObj.internet().emailAddress();
+		String company = fakeObj.company().name();
+		String testPassword = fakeObj.internet().password();
+		HandlingExcel new_excel_handler = new HandlingExcel();
+		new_excel_handler.Open_excel_sheet();
+		new_excel_handler.write_data_into_excel(0, "1", gender, first_name, Last_name, theDay, TheMonth, theYear, email, company, testPassword);
+		
+	}
+	
+	@DataProvider(name= "excelData")
+	public Object[][] userRegisterData() throws IOException{
+		HandlingExcel er = new HandlingExcel();
+		return er.getExcelData();
+	}
+	
+	@SuppressWarnings("static-access")
+	@Test(dependsOnMethods = {"add_data_to_excel"}, priority = 2, dataProvider = "excelData") 
+	private void RegisterNewUserWithCorrectCredentialWithExcel(String gender, String first_name, String Last_name, String theDay, String TheMonth, String theYear, String email, String company, String testPassword) {
+		reg_page_obj = new RegisterPage(testBaseDriver);
+
+		reg_page_obj.registerNewUser(gender, first_name, Last_name, theDay, Integer.parseInt(TheMonth), theYear, email, company, testPassword);
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("register-continue")));
+		assertTrue(reg_page_obj.registerationSuccessMsg.getText().contentEquals("Your registration completed"));
+	}
+
 
 
 
